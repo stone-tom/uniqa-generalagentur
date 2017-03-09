ab9ff89cf9c72cf85f4c481b4645ffaf2002d0a4
Thomas Steiner <stone.tom.email@gmail.com>
2017-03-10 00:21:06 +0100

    ordner structure and swiper test

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        direction: 'vertical',
+        mousewheelControl: true
+    });
+
+});
